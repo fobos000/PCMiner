a112059e0b078324bcb787742b72f76790f9dcf5
Ostap Horbach <ostap.horbach@admins-Mac-mini-2.local>
2015-04-17 12:30:16 +0300

    Initial Commit

---
+++ PCMiner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PCMiner
+//
+//  Created by Ostap Horbach on 4/17/15.
+//  Copyright (c) 2015 Ostap Horbach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PCMiner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PCMiner
+//
+//  Created by Ostap Horbach on 4/17/15.
+//  Copyright (c) 2015 Ostap Horbach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
